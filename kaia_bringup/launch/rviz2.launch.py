e7292ea19562f4e4713e3a5998677270764714a4
Ilia O <iliao@remake.ai>
2023-08-06 04:35:57 +0000

    Rviz launches

--- kaia_bringup/launch/rviz2.launch.py
+++ kaia_bringup/launch/rviz2.launch.py
@@ -25,7 +25,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     rviz_config_dir = os.path.join(
-        get_package_share_directory('kaia_description'),
+        get_package_share_directory('kaia_desc'),
         'rviz',
         'model.rviz')
 
