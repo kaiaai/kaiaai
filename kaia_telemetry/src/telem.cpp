872f136a22957463c64e16f4349db24afd914a9c
Ilia O <iliao@remake.ai>
2023-08-10 06:34:01 +0000

    Copyright

--- kaia_telemetry/src/telem.cpp
+++ kaia_telemetry/src/telem.cpp
@@ -1,12 +1,23 @@
-// https://automaticaddison.com/create-a-publisher-and-subscriber-in-c-ros-2-foxy-fitzroy/
+// Copyright 2023 REMAKE.AI
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <functional>
 #include <memory>
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-//#include "std_msgs/msg/string.hpp"
 #include "kaia_msgs/msg/kaia_telemetry.hpp"
-//#include <geometry_msgs/msg/twist.hpp>
 #include <builtin_interfaces/msg/time.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
