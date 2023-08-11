fc5d7da9ce6b3561e6bb39231bc2672ba617e9ad
Ilia O <iliao@remake.ai>
2023-08-11 07:51:49 +0000

    Rviz2 launch per model, package

--- kaia_bringup/launch/rviz2.launch.py
+++ kaia_bringup/launch/rviz2.launch.py
@@ -17,23 +17,46 @@
 # ACKNOWLEDGEMENTS: This code is based on ROBOTIS Turtlebot3
 
 import os
-
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription, LaunchContext
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+
+
+def make_robot_description_node(context: LaunchContext, model_name, package_name):
+    model_name_str = context.perform_substitution(model_name)
+    package_name_str = context.perform_substitution(package_name)
+
+    rviz_config_path_str = os.path.join(
+        get_package_share_path(package_name_str),
+        'rviz',
+        model_name_str + '.rviz')
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_path_str],
+        output='screen')
+
+    return [rviz2_node]
 
 
 def generate_launch_description():
-    rviz_config_dir = os.path.join(
-        get_package_share_directory('kaia_description'),
-        'rviz',
-        'model.rviz')
+    default_model_name = os.getenv('KAIA_BOT_MODEL', default='kaia_snoopy')
+    default_package_name = os.getenv('KAIA_BOT_PACKAGE', default='kaia_description')
+
+    package_name_arg = DeclareLaunchArgument(name='package', default_value=str(default_package_name),
+                                             description='Robot description package name, overrides KAIA_BOT_PACKAGE')
+    model_name_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_name),
+                                           description='Robot model name, overrides KAIA_BOT_MODEL')
 
     return LaunchDescription([
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            output='screen'),
+        model_name_arg,
+        package_name_arg,
+        OpaqueFunction(function=make_robot_description_node, args=[
+            LaunchConfiguration('model'),
+            LaunchConfiguration('package')
+        ])
     ])
